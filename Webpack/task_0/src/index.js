e14fdf4cf7474252e4a0cf3d7a500fff869d9f46
hayes28 <heather.hayes918@gmail.com>
2024-01-02 22:14:19 -0600

    Refactor code to append <p> elements using document.ready() function

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,5 +1,8 @@
 import $ from 'jquery';
 
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the students</p>");
-$('body').append("<p>Copyright - Holberton school</p>");
+// Append <p> elements to the <body> of the HTML
+$(document).ready(function() {
+    $('body').append("<p>Holberton Dashboard</p>");
+    $('body').append("<p>Dashboard data for the students</p>");
+    $('body').append("<p>Copyright - Holberton school</p>");
+});
