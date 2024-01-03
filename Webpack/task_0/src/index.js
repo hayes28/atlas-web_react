7b7a5ac1e3de7c721315a5ce619d47ac18c58a03
hayes28 <heather.hayes918@gmail.com>
2024-01-02 22:19:55 -0600

    Update text in index.js file, Had 's' not 'S'

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 // Append <p> elements to the <body> of the HTML
 $(document).ready(function() {
-    $('body').append("<p>Holberton Dashboard</p>");
-    $('body').append("<p>Dashboard data for the students</p>");
-    $('body').append("<p>Copyright - Holberton school</p>");
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
 });
