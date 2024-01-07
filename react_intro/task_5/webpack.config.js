c1ede6a1bc82628e6d258c49cc39cda8a2ce25eb
hayes28 <heather.hayes918@gmail.com>
2024-01-07 15:07:04 -0600

    Task 8 Add React Intro files with webpack

---
+++ react_intro/task_5/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "React Intro",
+        }),
+    ],
+    devServer: {
+        contentBase: "./dist",
+        hot: true,
+    },
+    devtool: "inline-source-map",
+};
