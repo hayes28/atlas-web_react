078a24e8c2c74a0f7369f9d8b0dbe4a1c3a51cf6
hayes28 <heather.hayes918@gmail.com>
2024-01-07 13:53:53 -0600

    Task 4 and 5 install enzyme

---
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,26 @@
+// Write a test to check that the function getFullYear returns the correct year (be careful to not create a time bomb)
+import { getFullYear } from './utils';
+import { getFooterCopy } from './utils';
+import { getLatestNotification } from './utils';
+
+describe('getFullYear', () => {
+    it('returns correct year', () => {
+        expect(getFullYear()).toEqual(new Date().getFullYear());
+    });
+    });
+    // Write a test to check that getFooterCopy returns the correct string when the argument is true or false
+    describe('getFooterCopy', () => {
+        it('returns correct string when argument is true', () => {
+            expect(getFooterCopy(true)).toEqual('Holberton School');
+        });
+        it('returns correct string when argument is false', () => {
+            expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+        });
+    });
+
+    // Write a test to check that getLatestNotification returns the correct string
+    describe('getLatestNotification', () => {
+        it('returns correct string', () => {
+            expect(getLatestNotification()).toEqual('<strong>Urgent requirement</strong> - complete by EOD');
+        });
+    });
