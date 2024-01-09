648e2370d17ef3be882fe3bebdc2bb502394fd27
hayes28 <heather.hayes918@gmail.com>
2024-01-09 17:01:39 -0600

    Task 1 Add new images and update dependencies and tests

--- react_props/task_1/dashboard/src/App/App.test.js
+++ react_props/task_1/dashboard/src/App/App.test.js
@@ -1,25 +1,34 @@
 // App.test.js
-import React from "react";
-import { shallow } from "enzyme";
-import App from "./App";
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
 
 describe("App", () => {
     it("renders without crashing", () => {
         shallow(<App />);
     });
 
-    // it("renders a div with the class App-header", () => {
-    //     const wrapper = shallow(<App />);
-    //     expect(wrapper.find(".App-header").exists()).toBe(true);
-    // });
+    it("contains the Notifications component", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.containsMatchingElement(<Notifications />)).toEqual(true);
+    });
 
-    // it("renders a div with the class App-body", () => {
-    //     const wrapper = shallow(<App />);
-    //     expect(wrapper.find(".App-body").exists()).toBe(true);
-    // });
+    it("contains the Header component", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.containsMatchingElement(<Header />)).toEqual(true);
+    });
 
-    // it("renders a div with the class App-footer", () => {
-    //     const wrapper = shallow(<App />);
-    //     expect(wrapper.find(".App-footer").exists()).toBe(true);
-    // });
+    it("contains the Login component", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.containsMatchingElement(<Login />)).toEqual(true);
+    });
+
+    it("contains the Footer component", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.containsMatchingElement(<Footer />)).toEqual(true);
+    });
 });
---
+++ react_props/task_1/dashboard/src/Login/Login.test.js
@@ -0,0 +1,25 @@
+// Login.test.js
+import React from 'react';
+import { shallow } from 'enzyme';
+import Login from './Login';
+
+describe('Login', () => {
+    it('renders without crashing', () => {
+        shallow(<Login />);
+    });
+
+    it('renders Login text', () => {
+        const wrapper = shallow(<Login />);
+        expect(wrapper.text().includes('Login')).toBe(true);
+    });
+
+    it('renders 2 input tags', () => {
+        const wrapper = shallow(<Login />);
+        expect(wrapper.find('input')).toHaveLength(2);
+    });
+
+    it('renders 2 label tags', () => {
+        const wrapper = shallow(<Login />);
+        expect(wrapper.find('label')).toHaveLength(2);
+    });
+});
