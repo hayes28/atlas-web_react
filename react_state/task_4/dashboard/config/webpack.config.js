4ece8d15bcdb91b182ef8d8aee82a98cfbfd0745
hayes28 <heather.hayes918@gmail.com>
2024-01-21 22:58:57 -0600

    Add new images and update webpack configuration

--- react_state/task_4/dashboard/config/webpack.config.js
+++ react_state/task_4/dashboard/config/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: path.resolve(__dirname, '../src/index.js'),
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "../dist"),
+        path: path.resolve(__dirname, "../build"),
     },
     module: {
         rules: [
